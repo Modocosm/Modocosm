f9f292225e4e351ac63b2c3f726484361668a1ad
JugaadiTech <william@modocosm.io>
2023-10-19 23:28:59 +0530

    Comment Out Directus

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
-import { createDirectus, rest, graphql } from '@directus/sdk';
+// import { createDirectus, rest, graphql } from '@directus/sdk';
 const client = createDirectus('http://directus.example.com');
 
 // https://astro.build/config
