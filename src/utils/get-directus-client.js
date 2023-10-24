a2021cebc3227b3e53c4ee4adabcdf059d23fad1
JugaadiTech <william@modocosm.io>
2023-10-25 00:54:22 +0530

    1

--- src/utils/get-directus-client.js
+++ src/utils/get-directus-client.js
@@ -1,18 +1,5 @@
-import { Directus } from "@directus/sdk";
-
-export const getDirectusClient = async () => {
-  const directus = new Directus(import.meta.env.PUBLIC_DIRECTUS_URL);
-
-  if (directus.auth.token) return directus;
-
-  if (import.meta.env.DIRECTUS_EMAIL && import.meta.env.DIRECTUS_PASSWORD) {
-    await directus.auth.login({
-      email: import.meta.env.DIRECTUS_EMAIL,
-      password: import.meta.env.DIRECTUS_PASSWORD,
-    });
-  } else if (import.meta.env.DIRECTUS_STATIC_TOKEN) {
-    await directus.auth.static(import.meta.env.DIRECTUS_STATIC_TOKEN);
-  }
-
-  return directus;
-};
+import { createDirectus, authentication, rest, } from "@directus/sdk";
+const email = 'info@modocosm.io';
+const password = '3G@&Lx3r#GKLen4q';
+export const client = createDirectus('https://modocosm.up.railway.app/').with(authentication()).with(rest());
+await client.login(email, password);
